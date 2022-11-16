a88d1a02d14a6887f180a7a210d33bb815b87cac
Bochanikita <bochanikita@yandex.ru>
2022-11-16 22:41:12 +0300

    Add Task9.6.1

---
+++ Task9.6.1/MyException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9._6._1
+{
+    public class MyException : Exception
+    {
+        public MyException()
+        {
+        }
+
+        public MyException(string message) : base(message)
+        {
+        }
+
+    }
+}
--- Task9.6.1/Program.cs
+++ Task9.6.1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Task9._6._1;
+
+Exception[] exceptions = new Exception[5];
+exceptions[0] = new DivideByZeroException();
+exceptions[1] = new MyException("Some exception!"); //My exception!
+exceptions[2] = new OverflowException();
+exceptions[3] = new TimeoutException();
+exceptions[4] = new DivideByZeroException();
+
+
+for (int i = 0; i < exceptions.Length; i++)
+{
+    try
+    {
+        throw exceptions[i];
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+    }
+}
